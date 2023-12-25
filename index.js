f81e2d3dfd46f44e49f1eccc525c42d570b1738e
gcscreations <gcscreations.ar.game@gmail.com>
2023-12-25 11:15:03 +0530

    RemoteAuth

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
 const express = require('express');
 const app = express();
 const port=3000;
-const { Client,LocalAuth,MessageMedia } = require('whatsapp-web.js');
-const { Server } = require("socket.io");
+const { Client,LocalAuth,MessageMedia ,RemoteAuth} = require('whatsapp-web.js');
+const mongoose = require('mongoose');
+const { MongoStore } = require('wwebjs-mongo');
+let store;
 const bodyParser = require('body-parser');
-
+const MONGODB_URI="mongodb+srv://selvapandigamedev95:Z7Ts6Q1R8ABF0WFD@cluster0.3vdhym6.mongodb.net/"
 const http = require('http');
 const server = http.createServer(app);
-const cors = require("cors");
-const io = new Server(server,{
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    },
-})
-server.listen(3000, () => {
-    console.log('listening on *:3000');
-});
-
-
-
-// Cors
-/*app.use((req, res, next) => {
-    /!*
-        res.header("Access-Control-Allow-credentials", "true");
-    *!/
-    res.header("Acces-Control-Allow-Origin", "*");
-    /!*res.header(
-        "Acces-Control-Allow-Header",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token"
-    );*!/
-    res.header('Acces-Control-Allow-Methods', "GET,OPTIONS, POST, DELETE, PUT, PATCH");
-
-    next();
-})*/
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-app.get('/jquery', (req, res) => {
-    res.sendFile(__dirname + '/public/jquery.min.js');
-});
 app.get('/Hello', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
@@ -48,7 +17,7 @@ app.get('/sendMessage', async (req, res) => {
     const number = req.query.number;
     const message = req.query.message;
     const mediaPath = req.query.mediaPath;
-    console.log(id,number,message,mediaPath)
+    console.log(id,number)
     //selva(to,message)
     //const { id, number, message,mediaPath } = data;
     const client = allsessionsObject[id];
@@ -65,11 +34,9 @@ app.get('/sendMessage', async (req, res) => {
     //console.log('chats',chats);
     //console.log('data',data);
     /*const messages = await Promise.all(number.map(async number => {
-
         const msg = await client.sendMessage(`${number}@c.us`, message, mediaOptions);
     }))
         socket.emit('sendMessageSuccess', { message: 'Message sent successfully',msg:messages });
-
 */
     const msg = await client.sendMessage(`${number}@c.us`, message, mediaOptions);
     res.send("msg sent");
@@ -90,17 +57,37 @@ app.get('/sendMessage', async (req, res) => {
 
 });
 
+mongoose.connect(MONGODB_URI).then(() => {
+    console.log("Hello To connnected MONGODB")
+     store = new MongoStore({ mongoose: mongoose });
+    console.log(store)
+    /*const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
 
+    client.initialize();*/
+});
+const { Server } = require("socket.io");
+const io = new Server(server,{
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    },
+})
+app.use(bodyParser.json());
 
 // app.get('/', (req, res) => {
 //   res.send('<h1>Hello world</h1>');
 // });
 
-
-app.use(bodyParser.json());
-app.use(cors());
-/*app.listen(5001, () => {
-    console.log("listening port " + port + "\nurl: http://localhost:" + 5001);
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+/*app.listen(port, () => {
+    console.log("listening port " + port + "\nurl: http://localhost:" + port);
 });*/
 
 const allsessionsObject = {};
@@ -115,11 +102,9 @@ const allsessionsObject = {};
         }
     ),
 });
-
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
 });
-
 client.on('ready', () => {
     console.log('Client is ready!');
 }); */
@@ -127,11 +112,12 @@ const createWhatsappSession = async (id,socket) => {
     let client = new Client({
         puppeteer:{
             headless:true,
-            args: ["--no-sandbox"],
         },
-        authStrategy: new LocalAuth(
+        authStrategy: new RemoteAuth(
             {
-                clientId:id
+                clientId:id,
+                store: store,
+                backupSyncIntervalMs: 300000
             }
         ),
     });
@@ -155,20 +141,22 @@ const createWhatsappSession = async (id,socket) => {
         allsessionsObject[id] = client;
         socket.emit('ready',{id,message:'client is ready'});
     });
-   /*  client.on('remote_session_saved',()=>{
-        console.log('remote-session saved');
-        socket.emit('remote_session_saved',{
-            message:'remote session saved'
-        });
-    }) */
+      client.on('remote_session_saved',()=>{
+         console.log('remote-session saved');
+         socket.emit('remote_session_saved',{
+             message:'remote session saved'
+         });
+     })
 
 
     client.initialize();
 }
-/*const getWhatsappSession = async (id,socket) => {
+const getWhatsappSession = async (id,socket) => {
+    console.log('getWhatsappSession is READY');
+    console.log(id);
     const client = new Client({
         puppeteer: {
-            headless: true,
+            headless: false,
         },
         authStrategy: new RemoteAuth({
             clientId: id,
@@ -178,99 +166,97 @@ const createWhatsappSession = async (id,socket) => {
     });
     client.on('ready', () => {
         console.log('wa-client is READY');
+        allsessionsObject[id] = client;
+        socket.emit('ready',{
+            id,
+            message:'client is ready'
+        });
     })
-    socket.emit('ready',{
-        id,
-        message:'client is ready'
-    });
+
     client.on('qr', (qr) => {
         // Generate and scan this code with your phone
         console.log('QR RECEIVED', {qr});
         socket.emit('qr',{qr,message:'QR RECEIVED when logged out'});
     });
     client.initialize();
-};*/
+};
 io.on('connection', (socket) => {
     console.log('a user connected',socket?.id);
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('user disconnected');
     });
     socket.on("connected",(data)=>
-  {
-    console.log("Connnected to the server",data)
-    socket.emit("Hello From the server");
-  });
-  socket.on('createSession', (data) => {
-      let count=0;
-      count++;
-      console.log('creating session for a user count', count);
+    {
+        console.log("Connnected to the server",data)
+        socket.emit("Hello From the server");
+    });
+    socket.on('createSession', (data) => {
+        console.log('creating session for a user', data);
+        const { id } = data;
+        createWhatsappSession(id, socket)
+            .then(() => {
+                const successMessage = "Session created successfully";
+                socket.emit('sessionCreated', { message: successMessage });
+            })
+            .catch((error) => {
+                const errorMessage = "Failed to create session";
+                socket.emit('sessionCreationFailed', { message: errorMessage });
+            });
+    });
+    socket.on('getSession', (data) => {
+        console.log('getSession',data);
+        const {id} = data;
+        getWhatsappSession(id,socket);
+    });
 
-      console.log('creating session for a user', data);
-    const { id } = data;
-    createWhatsappSession(id, socket)
-      .then(() => {
-        const successMessage = "Session created successfully";
-        socket.emit('sessionCreated', { message: successMessage });
-      })
-      .catch((error) => {
-        const errorMessage = "Failed to create session";
-        socket.emit('sessionCreationFailed', { message: errorMessage });
-      });
-  });
+    socket.on('getAllChats',async (data)=>{
+        console.log('getting all chats',data);
+        const {id} = data;
+        const client = allsessionsObject[id];
+        const chats = await client.getChats();
+        socket.emit('allChats',{chats});
+    })
 
+    socket.on('sendMessage', async (data) => {
+        console.log('sending message', data);
+        const { id, number, message,mediaPath } = data;
+        const client = allsessionsObject[id];
 
-socket.on('getAllChats',async (data)=>{
-    console.log('getting all chats',data);
-    /*const {id} = data;
-    const client = allsessionsObject[id];
-    const chats = await client.getChats();*/
-    //socket.emit('allChats',{chats});
-    socket.emit('allChats',{data});
-})
-
-socket.on('sendMessage', async (data) => {
-    console.log('sending message', data);
-    const { id, number, message,mediaPath } = data;
-    const client = allsessionsObject[id];
-
-    let mediaOptions = {};
-    if (mediaPath) {
-        const mediaFile = await MessageMedia.fromUrl(mediaPath)
-        mediaOptions = {
-            caption: 'This is a media caption',
-            media: mediaFile
-        };
-    }
-    const chats = await client.getChats();
-    console.log('chats',chats);
-    console.log('data',data);
-    /*const messages = await Promise.all(number.map(async number => {
-
+        let mediaOptions = {};
+        if (mediaPath) {
+            const mediaFile = await MessageMedia.fromUrl(mediaPath)
+            mediaOptions = {
+                caption: 'This is a media caption',
+                media: mediaFile
+            };
+        }
+        const chats = await client.getChats();
+        console.log('chats',chats);
+        console.log('data',data);
+        /*const messages = await Promise.all(number.map(async number => {
+            const msg = await client.sendMessage(`${number}@c.us`, message, mediaOptions);
+        }))
+            socket.emit('sendMessageSuccess', { message: 'Message sent successfully',msg:messages });
+    */
         const msg = await client.sendMessage(`${number}@c.us`, message, mediaOptions);
-    }))
-        socket.emit('sendMessageSuccess', { message: 'Message sent successfully',msg:messages });
-
-*/
-    const msg = await client.sendMessage(`${number}@c.us`, message, mediaOptions);
-    socket.emit('sendMessageSuccess', { message: 'Message sent successfully',msg:msg });
+        socket.emit('sendMessageSuccess', { message: 'Message sent successfully',msg:msg });
 
 
 
-    // SendWaMessage(id, socket, numbers, message, mediaPath)
-    //   .then(() => {
-    //     const successMessage = "message sent successfully";
-    //     socket.emit('sessionCreated', { message: successMessage });
-    //   })
-    //   .catch((error) => {
-    //     const errorMessage = "Failed to send message";
-    //     socket.emit('mesage sent failed', { message: errorMessage });
-    //   });
-  });
+        // SendWaMessage(id, socket, numbers, message, mediaPath)
+        //   .then(() => {
+        //     const successMessage = "message sent successfully";
+        //     socket.emit('sessionCreated', { message: successMessage });
+        //   })
+        //   .catch((error) => {
+        //     const errorMessage = "Failed to send message";
+        //     socket.emit('mesage sent failed', { message: errorMessage });
+        //   });
+    });
     function selva(id,number)
     {
         console.log("Message Called Function:"+id,name);
     }
-  });
+});
 
 //client.initialize();
-
